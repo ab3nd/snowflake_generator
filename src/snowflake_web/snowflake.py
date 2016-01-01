1ee93d7d58fc4e11ecbe75593a4b1fc91e3d8f23
Abe Shultz <orphrey@gmail.com>
2015-12-31 21:02:29 -0500

    Now with Jinja template powah! Also, ready to deploy.

--- src/snowflake_web/snowflake.py
+++ src/snowflake_web/snowflake.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 import re
 import flake_generator
 
@@ -13,12 +13,13 @@ def genSVG(user_string):
     #Strip out everything that's not a letter
     user_string = user_string.lower()
     user_string = re.sub("[^a-z]", "", user_string)
-    #Cut off all but the first 10 characters
-    user_string = user_string[:10]
+    #Cut off all but the first 20 characters
+    user_string = user_string[:20]
     #Generate the snowflake
     flake_generator.single_flake_png("./static/", user_string)   
     #Get the url to it
     pic_url = url_for('static', filename='{0}.png'.format(user_string))
+    return render_template('snowflake.html', img_path=pic_url, input_string=user_string)
     return "<html><head></head><body><img src=\"{0}\" alt=\"A snowflake\"></body></html>".format(pic_url)
     
 if __name__ == "__main__":
