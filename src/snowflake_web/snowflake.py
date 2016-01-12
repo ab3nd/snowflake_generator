621678558cf739c3e822408a8a74fcb9be804819
ams <abrshu@snowstorm.dreamhost.com>
2016-01-12 11:34:37 -0800

    Fixed path to generate snowflake files in the proper place

--- src/snowflake_web/snowflake.py
+++ src/snowflake_web/snowflake.py
@@ -16,7 +16,7 @@ def genPNG(user_string):
     #Cut off all but the first 20 characters
     user_string = user_string[:20]
     #Generate the snowflake
-    flake_generator.single_flake_png("./static/", user_string)   
+    flake_generator.single_flake_png("/home/abrshu/snowflake.gizmosmith.com/snowflake_gen/src/snowflake_web/static/", user_string)   
     #Get the url to it
     pic_url = url_for('static', filename='{0}.png'.format(user_string))
     return render_template('snowflake.html', img_path=pic_url, input_string=user_string)
